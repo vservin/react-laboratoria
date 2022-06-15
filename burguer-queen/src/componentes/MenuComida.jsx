48f628d7035d04867800879d5d11e23facb613dd
Felipe Guizar <fguizar@etsy.com>
2022-06-15 00:18:25 -0500

    create productos component

--- burguer-queen/src/componentes/MenuComida.jsx
+++ burguer-queen/src/componentes/MenuComida.jsx
@@ -5,13 +5,7 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import Productos from "./Productos";
 
 const MenuComida = () => {
   const [activeTab, setActiveTab] = useState("1"); 
@@ -30,27 +24,7 @@ const MenuComida = () => {
           </TabList>
         </Box>
         <TabPanel value="1">
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://images.unsplash.com/photo-1522992319-0365e5f11656?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Café americano
-              </Typography>
-              <Typography variant="body2" color="text.secondary" sx={{ mb: 2 }}>
-                Muy bueno y barato :)
-              </Typography>
-              <strong> 35.00 MXN</strong>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Comprar</Button>
-              <Button size="small">Detalles</Button>
-            </CardActions>
-          </Card>
+          <Productos type="desayunos"/>
         </TabPanel>
         <TabPanel value="2">Almuerzo y Cena</TabPanel>
       </TabContext>
